002584ddf88215fbb5a255168900ccf7efb64ed3
Maratib Ali Khan <maratib@gmail.com>
2023-12-03 22:01:19 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Render1 />
+  </div>
+</template>
+<script setup lang="ts">
+import Render1 from './pages/Render1.vue';
+
+</script>
+
+
+<style scoped></style>
---
+++ src/pages/Render1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Render 1</h1>
+    <render />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { h } from 'vue';
+import Foo from '@/components/Foo.vue'
+import Bar from '@/components/Bar.vue'
+
+
+const render = () => {
+  return h('div', [h(Foo), h(Bar)])
+}
+
+</script>
+
